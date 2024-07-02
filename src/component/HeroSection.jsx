67243772446280013ccc2ecb65405f1e69794d74
dspatel01 <pateldileep290492@gmail.com>
2024-07-02 20:27:01 +0530

    img testing

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import img from '../assets/cover.avif'
 
 
 const HeroSection = () => {
@@ -6,6 +7,7 @@ const HeroSection = () => {
         <>
 
             <div className=' bg-gray-500 w-full xl:h-[80vh] lg:h-[80vh] md:h-[60vh] sm:h-[50vh] h-[50vh]'>
+                <img className=' w-full xl:h-[80vh] lg:h-[80vh] md:h-[60vh] sm:h-[50vh] h-[50vh]' src={img} alt="" />
             </div>
           
         </>
