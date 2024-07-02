8d11245a894de7318c950d0587baac485afb2d6b
dspatel01 <pateldileep290492@gmail.com>
2024-07-02 22:08:33 +0530

    responsie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,67 +15,58 @@ const Home = () => {
 
         <div className='w-full flex items-center justify-center mt-10 mb-20 '>
 
-                <div className='w-[100%] flex justify-around flex-wrap mx-4 '>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-                    <div className=''>
-                        <div className=' my-2 mx-2 w-64 h-80 shadow-lg rounded-xl cursor-pointer'>
-                            <img src={img} alt="" className='rounded-xl w-64 h-80' />
-
-                        </div> 
-                       
-                    </div>
-
-                </div>
+          <div className='w-[100%] flex justify-around flex-wrap mx-4 '>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
             </div>
-      </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+            <div className='bg-red-800 xl:w-80 lg:w-80 md:w-[45%] w-full rounded-xl my-2 mx-0 '  >
+              <div className='xl:w-80 lg:w-80 md:w-[100%] sm:mx-0 h-[350px] w-[100%] shadow-lg rounded-xl cursor-pointer bg-red-50'>
+                <img src={img} alt="" className=' w-[100%] h-[100%] rounded-xl ' />
+              </div>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div >
     </>
   )
 }
